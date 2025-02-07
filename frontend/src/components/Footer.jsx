037969f62bf52c30a85cbd7295eca7bbc7bdd852
akankshhaa <93974771+akankshhaa@users.noreply.github.com>
2025-02-08 01:28:43 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-20 text-sm">
+        <div className="flex flex-col justify-start">
+          <img src={assets.logo} className="mb-2 w-32" alt="Logo" />
+          <p className="w-full md:w-2/3 text-gray-600 mt-2">
+            Bazario Shop is a leading e-commerce platform that provides quality
+            products across various categories including fashion, electronics, and home essentials. 
+            We strive to offer the best prices and customer service in the industry.
+          </p>
+        </div>
+
+        <div className="flex flex-col justify-start">
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+  <li>
+    <a href="tel:+919876543210" className="hover:text-black">
+      +91-98765 43210
+    </a>
+  </li>
+  <li>
+    <a href="mailto:support@bazarioshop.com" className="hover:text-black">
+      support@bazarioshop.com
+    </a>
+  </li>
+</ul>
+
+        </div>
+
+        <div className="flex flex-col justify-start">
+          <p className="text-xl font-medium mb-5">FOLLOW US</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>
+              <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-black">
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-black">
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-black">
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+
+      <div>
+        <hr/>
+        <p className="py-5 text-sm text-center">Copyright 2024@ akankshhaa - All Right Reserved.</p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NewsLetterBox = () => {
+
+    const onSubmitHandler = (event) =>{
+        event.preventDefault();
+        alert("🚨 Oops! Currently offer is not available.");
+    }
+
+  return (
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-800'>Check available offers here!!!</p>
+        <p className='text-gray-400 mt-3'>Stay updated with the latest trends and exclusive offers. We bring you the best deals! 🌟</p>
+        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+            <input className='w-full sm:flex-1 outline-none' type='email' placeholder='Enter your email'required/>
+            <button type='submit' className='bg-black text-white text-xs px-10 py-4'>CHECK OFFER</button>
+        </form>
+    </div>
+  )
+}
+
+export default NewsLetterBox
